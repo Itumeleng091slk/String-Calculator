03d471cc7f347ae4dcac19d19f2af0bff6e82c8c
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-07-13 11:45:51 +0200

    Update calculator.py

--- string_Calculator/calculator.py
+++ string_Calculator/calculator.py
@@ -4,39 +4,51 @@ import re
 regex = re.compile(r'\d+')
 
 def add(string):
-    string = delimiters(string)
-    if string:
-        return add_numbers_into(string)
-    else:
-        return 0
+    sum = 0
+    numbers = regex.findall(string)
+    negatives = calc_has_negatives(string)
+    delimiter = ","
+    if string.startswith('//'):
+        delimiter = string[2]
+        string = string[3:]
+    string = string.replace('\n',delimiter)
+    string_num = string.split(delimiter)
+    sum_of_number = []
+    negative_nums = []
 
-def delimiters(string):
-    string = multiple_delimiters(string)
-    string = string.replace('\n', ',')
-    sum = []
-    numbers = regex.findall(string) # this line is telling the program to find all the regular expressions
-    negatives = validate_negative_numbers(string)
-    return string
+    for number in string_num:
+        try:
+            numbers = int(number)
+            if int(number) < 0:
+                negative_nums.append(numbers)
+        except ValueError:
+            numbers = 0
+        sum_of_number.append(numbers)
+        
+    if negative_nums:
+        message = ','.join([str(numbers) for numbers in negative_nums])
+        raise Exception('Negatives not allowed'+ message)
+    return sum    
+
+       
+def calc_has_negatives(string):
+    empty_string = ''
+    for x in range(len(string)):
+        if string[x] == '-' and string[x+1].isdecimal():
+            empty_string += '-' + string[x+1] + ','
+    return empty_string
 
 def multiple_delimiters(string):
     if string.startswith('//'):
-        delimiter_rule, string = string.split('\n', 1)
+        delimiter_rule, string_multi = string.split('\n', 1)
         delimiter = delimiter_rule[2:]
         string = string.replace(delimiter, ',')
     return string
 
 def add_numbers_into(string):
     numbers = map(int, string.split(','))
-    validate_negative_numbers(numbers)
+    add(numbers)
     return sum(numbers)
 
-def validate_negative_numbers(numbers):
-    while True:
-        try:
-            any(number < 0 for number in numbers)
-            break
-        except ValueError:
-            print("negatives are not allowed")
 
-print(delimiters("//4\n142"))
-print(add("//;\n1;2"))
+print(add("-1,-2,3,4"))
